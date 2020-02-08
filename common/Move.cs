cafa4661af9a24dbbb641f5221310c69b28f4fef
Nuno Fachada <faken@fakenmc.com>
2020-02-08 21:25:19 +0000

    First commit for common code

---
+++ common/Move.cs
@@ -0,0 +1,33 @@
+/// @file
+/// @brief This file contains the ::Move struct.
+///
+/// @author Nuno Fachada
+/// @date 2019, 2020
+/// @copyright [MPLv2](http://mozilla.org/MPL/2.0/)
+
+namespace ColorShapeLinks.Common
+{
+    /// <summary>Represents a move already performed.</summary>
+    public struct Move
+    {
+        /// <summary>Board row where the piece was placed.</summary>
+        public readonly int row;
+
+        /// <summary>Board column where the piece was placed.</summary>
+        public readonly int col;
+
+        /// <summary>Piece used for the move.</summary>
+        public readonly Piece piece;
+
+        /// <summary>Create a move.</summary>
+        /// <param name="row">Board row where the piece was placed.</param>
+        /// <param name="col">Board column where the piece was placed.</param>
+        /// <param name="piece">Piece used for the move.</param>
+        public Move(int row, int col, Piece piece)
+        {
+            this.row = row;
+            this.col = col;
+            this.piece = piece;
+        }
+    }
+}
