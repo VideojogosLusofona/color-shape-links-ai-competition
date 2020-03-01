c6b1a05bc3e015c263fe9f0301977002d2ed8712
Nuno Fachada <faken@fakenmc.com>
2020-03-01 18:16:10 +0000

    Add useful PColor extensions

--- PColorExtensions.cs
+++ PColorExtensions.cs
@@ -20,5 +20,41 @@ namespace ColorShapeLinks.Common
         /// <returns>A formatted thinker name with color.</returns>
         public static string FormatName(this PColor color, string name) =>
             $"{name} ({color})";
+
+        /// <summary>
+        /// Returns the other color.
+        /// </summary>
+        /// <param name="color">This color.</param>
+        /// <returns>
+        /// * Returns ::PColor.White if this color is ::PColor.Red.
+        /// * Returns ::PColor.Red if this color is ::PColor.White.
+        /// </returns>
+        public static PColor Other(this PColor color) =>
+            color == PColor.White ? PColor.Red : PColor.White;
+
+        /// <summary>
+        /// Returns the shape associated with this player's color for
+        /// winning purposes.
+        /// </summary>
+        /// <param name="color">This color.</param>
+        /// <returns>
+        /// * Returns ::PShape.Round if color is PColor.White.
+        /// * Returns ::PShape.Square if color is PColor.Red.
+        /// </returns>
+        public static PShape Shape(this PColor color) =>
+            color == PColor.White ? PShape.Round : PShape.Square;
+
+        /// <summary>
+        /// Is the given piece a friend of this color? In other words, is the
+        /// given piece of the same color as this color or associated with the
+        /// same shape for winning purposes as this color?
+        /// </summary>
+        /// <param name="color">This color.</param>
+        /// <param name="piece">The piece to check for friendship.</param>
+        /// <returns>
+        /// `true` is given piece is a friend, `false` otherwise.
+        /// </returns>
+        public static bool FriendOf(this PColor color, Piece piece) =>
+            color == piece.color || color.Shape() == piece.shape;
     }
 }
