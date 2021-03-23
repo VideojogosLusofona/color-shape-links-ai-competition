3eea8cf3de19fe05a3bf215d3f77633b4578d7c2
Nuno Fachada <nuno.fachada@ulusofona.pt>
2021-03-23 00:29:54 +0000

    Sequential agent plays his shapes first

--- AI/Examples/SequentialAIThinker.cs
+++ AI/Examples/SequentialAIThinker.cs
@@ -12,8 +12,9 @@ namespace ColorShapeLinks.Common.AI.Examples
 {
     /// <summary>
     /// Implementation of an AI that will always play in sequence, from the
-    /// first to the last column. It will start by spending all round pieces,
-    /// and only then start using the square pieces.
+    /// first to the last column. It will start by using pieces with its winning
+    /// shape, and when these are over, it continues by playing pieces with the
+    /// losing shape.
     /// </summary>
     public class SequentialAIThinker : AbstractThinker
     {
@@ -38,15 +39,16 @@ namespace ColorShapeLinks.Common.AI.Examples
             }
             while (board.IsColumnFull(lastCol));
 
-            // Try to use a round piece first
-            if (board.PieceCount(board.Turn, PShape.Round) > 0)
+            // Try to use the winning shape first
+            if (board.PieceCount(board.Turn, board.Turn.Shape()) > 0)
             {
-                move = new FutureMove(lastCol, PShape.Round);
+                move = new FutureMove(lastCol, board.Turn.Shape());
             }
-            // If there's no round pieces left, let's try a square pieces
-            else if (board.PieceCount(board.Turn, PShape.Square) > 0)
+            // If there's no pieces with the winning shape left, try and use
+            // the other shape
+            else if (board.PieceCount(board.Turn, board.Turn.Other().Shape()) > 0)
             {
-                move = new FutureMove(lastCol, PShape.Square);
+                move = new FutureMove(lastCol, board.Turn.Other().Shape());
             }
             // Otherwise return a "no move" (this should never happen)
             else
