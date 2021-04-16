2f5fd4874e99e4b85c55b3badabb56a62cf572f6
Nuno Fachada <nuno.fachada@ulusofona.pt>
2021-04-16 23:41:00 +0100

    Documentation updates

--- docs/html/namespace_color_shape_links.js
+++ docs/html/namespace_color_shape_links.js
@@ -2,6 +2,7 @@ var namespace_color_shape_links =
 [
     [ "How to properly download the ColorShapeLinks development framework?", "faq.html#autotoc_md24", null ],
     [ "What are the software requirements for developing and testing an AI?", "faq.html#autotoc_md25", null ],
+    [ "How to cite the ColorShapeLinks competition?", "faq.html#autotoc_md26", null ],
     [ "Common", "namespace_color_shape_links_1_1_common.html", "namespace_color_shape_links_1_1_common" ],
     [ "TextBased", "namespace_color_shape_links_1_1_text_based.html", "namespace_color_shape_links_1_1_text_based" ],
     [ "UnityApp", "namespace_color_shape_links_1_1_unity_app.html", "namespace_color_shape_links_1_1_unity_app" ]
