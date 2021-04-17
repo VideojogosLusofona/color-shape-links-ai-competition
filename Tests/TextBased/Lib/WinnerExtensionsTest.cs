478b44b9832d0d030deb2c4031d01b377fd70945
Nuno Fachada <nuno.fachada@ulusofona.pt>
2021-04-17 19:39:28 +0100

    Tests: add tests for WinnerExtensions

---
+++ Tests/TextBased/Lib/WinnerExtensionsTest.cs
@@ -0,0 +1,33 @@
+/// @file
+/// @brief This file contains the ::ColorShapeLinks.Common.Board class.
+///
+/// @author Nuno Fachada
+/// @date 2021
+/// @copyright [MPLv2](http://mozilla.org/MPL/2.0/)
+
+using System.Collections.Generic;
+using Xunit;
+using ColorShapeLinks.Common;
+using ColorShapeLinks.TextBased.Lib;
+
+namespace Tests.Lib
+{
+    public class WinnerExtensionsTests
+    {
+        public static IEnumerable<object[]> GetRelations()
+        {
+            yield return new object[] { Winner.Draw, ExitStatus.Draw };
+            yield return new object[] { Winner.Red, ExitStatus.RedWins };
+            yield return new object[] { Winner.White, ExitStatus.WhiteWins };
+            yield return new object[] { int.MaxValue, ExitStatus.Exception };
+            yield return new object[] { -1, ExitStatus.Exception };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetRelations))]
+        public void ToExitStatus_ReturnExpected_Yes(Winner winner, ExitStatus es)
+        {
+            Assert.Equal(winner.ToExitStatus(), es);
+        }
+    }
+}
